69e09505bbc1932a5ee552673733d3c0e3ec1c76
yuvarajmteddy <myr7895@gmail.com>
2021-04-09 13:31:06 +0530

    taskdone

--- src/org/test/ProjectTest.java
+++ src/org/test/ProjectTest.java
@@ -1,39 +1,64 @@
 package org.test;
 
 public class ProjectTest {
-	
+
 	private void method1() {
 		// TODO Auto-generated method stub
 
 	}
+
 	private void method2() {
 		// TODO Auto-generated method stub
 
 	}
+
 	private void method3() {
 		// TODO Auto-generated method stub
 
 	}
+
 	private void method4() {
 		// TODO Auto-generated method stub
 
 	}
+
 	private void method5() {
 		// TODO Auto-generated method stub
 
 	}
+
 	private void method6() {
 		// TODO Auto-generated method stub
 
 	}
+
+	private void method10() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void method20() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void method30() {
+		// TODO Auto-generated method stub
+
+	}
+
 	public static void main(String[] args) {
-		ProjectTest test=new ProjectTest();
+		ProjectTest test = new ProjectTest();
 		test.method1();
 		test.method2();
 		test.method3();
 		test.method4();
 		test.method5();
 		test.method6();
+		test.method10();
+		test.method20();
+		test.method30();
+
 	}
 
 }
