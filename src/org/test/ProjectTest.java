f667fd727493c133f763deaac2c4bcc6b07478e8
PreethaJothi99 <preethajothi02@gmail.com>
2021-04-09 12:36:30 +0530

    message

---
+++ src/org/test/ProjectTest.java
@@ -0,0 +1,24 @@
+package org.test;
+
+public class ProjectTest {
+	
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		ProjectTest test=new ProjectTest();
+		test.method1();
+		test.method2();
+		test.method3();
+	}
+
+}
